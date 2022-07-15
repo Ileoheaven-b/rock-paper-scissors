a6d4a4f3839371fba9d390159960b0dc20073b39
Ileoheaven <ileoheaven.b@gmail.com>
2022-07-15 22:05:05 +0300

    Finish round system and score holder

--- page/javascript.js
+++ page/javascript.js
@@ -16,3 +16,55 @@ function getComputerChoice() {
     }
 }
 
+function playRound() {
+    playerSelection = prompt("Enter your choice");
+    playerSelection = playerSelection.toLowerCase();
+    getComputerChoice();
+    if (computerSelection == playerSelection) {
+        return "tie";
+    }
+    else if (computerSelection == "rock" && playerSelection == "paper") {
+        return "player";
+    }
+    else if (computerSelection == "rock" && playerSelection == "scissors") {
+        return "computer";
+    }
+    else if (computerSelection == "paper" && playerSelection == "rock") {
+        return "computer";
+    }
+    else if (computerSelection == "paper" && playerSelection == "scissors") {
+        return "player";
+    }
+    else if (computerSelection == "scissors" && playerSelection == "rock") {
+        return "player";
+    }
+    else return "computer"
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    let winner;
+    for (let i = 1; i <= 3; i++) {
+        winner = playRound();
+        console.log("for loop: " + i);
+        if (winner == "tie") {
+            console.log("Tie!");
+        }
+        else if (winner == "computer") {
+            computerScore++;
+            console.log("Lose!");
+        }
+        else if (winner == "player") {
+            playerScore++;
+            console.log("Win!");
+        }
+    }
+    if (computerScore > playerScore) {
+        alert("You lost! The score is: " + playerScore + " - " + computerScore);
+    }
+    else if (computerScore == playerScore) {
+        alert("Tie! The score is: " + playerScore + " - " + computerScore);
+    }
+    else alert("You Won! The score is: " + playerScore + " - " + computerScore);
+}
